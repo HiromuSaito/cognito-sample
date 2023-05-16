b82b1f0847c457a30ea86995f5fb730cd8e6143d
HiromuSaito <hiromu.sai10@gmail.com>
2023-05-16 22:58:01 +0900

    feat:一時認証情報の取得処理の実装

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,32 +1,69 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { SigninDto } from './dto/signinDto';
 import { Response } from 'express';
-import { AdminInitiateAuthCommand, CognitoIdentityProvider, CognitoIdentityProviderClient, ListUserPoolClientsCommand, ListUserPoolsCommand } from '@aws-sdk/client-cognito-identity-provider';
+import { AdminInitiateAuthCommand, CognitoIdentityProviderClient } from '@aws-sdk/client-cognito-identity-provider';
+import { CognitoIdentityClient, Credentials, GetCredentialsForIdentityCommand, GetIdCommand } from '@aws-sdk/client-cognito-identity';
 
 @Injectable()
 export class AuthService {
-    async signin(signinDto: SigninDto, res: Response) {
-        const cognitClinet = new CognitoIdentityProviderClient({
-            region: process.env.AWS_REGION
-        })
+  async signin(signinDto: SigninDto, res: Response) {
+    const cognitClinet = new CognitoIdentityProviderClient({
+      region: process.env.AWS_REGION
+    })
 
-        const command = new AdminInitiateAuthCommand({
-            UserPoolId: process.env.USER_POOL_ID,
-            ClientId: process.env.COGNITO_CLIENT_ID,
-            AuthFlow: 'ADMIN_NO_SRP_AUTH',
-            AuthParameters: {
-                USERNAME: signinDto.email,
-                PASSWORD: signinDto.password
-            },
-        })
-        await cognitClinet.send(command).
-            then((result) => {
-                res.cookie('id_token', result.AuthenticationResult?.IdToken)
-                res.cookie('access_token', result.AuthenticationResult?.AccessToken)
-            })
-            .catch(e => {
-                console.log(e)
-                throw new UnauthorizedException()
-            })
+    const command = new AdminInitiateAuthCommand({
+      UserPoolId: process.env.USER_POOL_ID,
+      ClientId: process.env.COGNITO_CLIENT_ID,
+      AuthFlow: 'ADMIN_NO_SRP_AUTH',
+      AuthParameters: {
+        USERNAME: signinDto.email,
+        PASSWORD: signinDto.password
+      },
+    })
+    await cognitClinet.send(command).
+      then((result) => {
+        res.cookie('id_token', result.AuthenticationResult?.IdToken)
+        res.cookie('access_token', result.AuthenticationResult?.AccessToken)
+      })
+      .catch(e => {
+        console.log(e)
+        throw new UnauthorizedException()
+      })
+  }
+
+  async createTemporalCredential(idToken: string): Promise<Credentials> {
+    const identityId = await this.getIdentityId(idToken);
+    const cognitClinet = new CognitoIdentityClient({
+      region: process.env.AWS_REGION
+    })
+    const command = new GetCredentialsForIdentityCommand({
+      IdentityId: identityId,
+      Logins: { [`cognito-idp.ap-northeast-1.amazonaws.com/${process.env.USER_POOL_ID}`]: idToken },
+    })
+
+    try {
+      return (await cognitClinet.send(command)).Credentials
+    } catch (e) {
+      console.log(e)
+      throw new UnauthorizedException()
     }
+  }
+
+  async getIdentityId(idToken: string): Promise<string> {
+    const cognitClinet = new CognitoIdentityClient({
+      region: process.env.AWS_REGION
+    })
+
+    const command = new GetIdCommand({
+      IdentityPoolId: process.env.COGNITO_ID_POOL,
+      Logins: { [`cognito-idp.ap-northeast-1.amazonaws.com/${process.env.USER_POOL_ID}`]: idToken },
+    })
+
+    try {
+      return (await cognitClinet.send(command)).IdentityId
+    } catch (e) {
+      console.error(e)
+      throw new UnauthorizedException()
+    }
+  }
 }
