2ab5d9a4376fb3131e720006177415d7b0236dbb
HiromuSaito <hiromu.sai10@gmail.com>
2023-05-21 15:45:56 +0900

    remove:不要となったファイル群を削除

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
 import { ConfigModule } from '@nestjs/config';
@@ -9,7 +7,7 @@ import { FileService } from './file/file.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController, AuthController, FileController],
-  providers: [AppService, AuthService, FileService],
+  controllers: [AuthController, FileController],
+  providers: [AuthService, FileService],
 })
-export class AppModule {}
+export class AppModule { }
