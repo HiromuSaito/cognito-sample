51795cce1e05d4798727d3c5d03902e66aec9af4
HiromuSaito <hiromu.sai10@gmail.com>
2023-05-22 21:56:59 +0900

    不要なconsole.logを削除

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  console.log('tdestd')
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableShutdownHooks()
